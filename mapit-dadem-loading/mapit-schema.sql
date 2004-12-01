f4b6d2b6bfefa369790d91ecf5514929381d4a75
chris <chris>
2004-12-01 01:24:11 +0000

    Uniqueness issues.

--- mapit-dadem-loading/mapit-schema.sql
+++ mapit-dadem-loading/mapit-schema.sql
@@ -5,7 +5,7 @@
 -- Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: mapit-schema.sql,v 1.6 2004-11-29 18:05:01 chris Exp $
+-- $Id: mapit-schema.sql,v 1.7 2004-12-01 01:24:11 chris Exp $
 --
 
 -- generations, for currency of data
@@ -45,7 +45,7 @@ create table area (
 );
 
 -- index these so that updates may be made.
-create unique index area_unit_id_idx on area(unit_id);
+--create unique index area_unit_id_idx on area(unit_id);
 create unique index area_ons_code_idx on area(ons_code);
 -- NB these are not unique, because two areas may be coterminous
 create index area_geom_hash_idx on area(geom_hash);
