faf1f11babf5aea9a4020d364c7fd0cf6c1cf2e4
chris <chris>
2005-05-10 13:52:46 +0000

    Remove "_table" from end of table name (we'll have no Hungarian notation here), and create indices. Everyone likes indices, even when they have names as long as these ones....

--- mapit-dadem-loading/new-scottish-constituencies-fixup-table.sql
+++ mapit-dadem-loading/new-scottish-constituencies-fixup-table.sql
@@ -6,11 +6,17 @@
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: new-scottish-constituencies-fixup-table.sql,v 1.1 2005-05-10 13:26:01 chris Exp $
+-- $Id: new-scottish-constituencies-fixup-table.sql,v 1.2 2005-05-10 13:52:46 chris Exp $
 --
 
-create table new_scottish_constituencies_fixup_table (
+create table new_scottish_constituencies_fixup (
     council_area_id integer references area(id),
     ward_area_id integer references area(id),
     constituency_area_id integer not null references area(id)
 );
+
+create unique index new_scottish_constituencies_fixup_ward_area_id_idx
+    on new_scottish_constituencies_fixup(ward_area_id);
+
+create unique index new_scottish_constituencies_fixup_council_area_id_idx
+    on new_scottish_constituencies_fixup(council_area_id);
