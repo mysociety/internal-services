3d02e749dbf1afe275b410d68052a6e6a4c93811
chris <chris>
2005-05-12 14:43:33 +0000

    Also update generation_low.

--- mapit-dadem-loading/new-scottish-constituencies-update-generations.sql
+++ mapit-dadem-loading/new-scottish-constituencies-update-generations.sql
@@ -5,13 +5,13 @@
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: new-scottish-constituencies-update-generations.sql,v 1.1 2005-05-12 12:00:33 chris Exp $
+-- $Id: new-scottish-constituencies-update-generations.sql,v 1.2 2005-05-12 14:43:33 chris Exp $
 --
 
 -- XXX should check actual value of generation_high
 update area set generation_high = 2 where country = 'E' or country = 'W';
 update area set generation_high = 2 where country = 'S' and type <> 'WMC';
-update area set generation_high = 2
+update area set generation_high = 2, generation_low = 2
 where country = 'S'
     and (
         select constituency_area_id from new_scottish_constituencies_fixup
