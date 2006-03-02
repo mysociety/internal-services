b4932ffa570ba41aab72d7f42c9e3dbd139ef09c
francis <francis>
2006-03-02 06:57:31 +0000

    XML interface for postcode lookup.

---
+++ web/mp-lookup-xml.php
@@ -0,0 +1,53 @@
+<?
+/*
+ * mp-lookup-xml.php:
+ * XML interface to postcode lookup.
+ * 
+ * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew@mysociety.org. WWW: http://www.mysociety.org
+ *
+ * $Id: mp-lookup-xml.php,v 1.1 2006-03-02 06:57:31 francis Exp $
+ * 
+ */
+require_once "../conf/general";
+require_once "../../phplib/utility.php";
+require_once "../../phplib/mapit.php";
+require_once '../../phplib/dadem.php';
+
+$pc = get_http_var('input_postcode');
+
+header("Content-Type: text/xml"); ?>
+<FAXYOURMP>
+<?
+
+$voting_areas = '';
+if ($pc != "") {
+    $voting_areas = mapit_get_voting_areas($pc);
+    if (!rabx_is_error($voting_areas)) {
+        $va_info = mapit_get_voting_areas_info(array_values($voting_areas));
+        if (!rabx_is_error($va_info)) {
+            $type_reps = array();
+            foreach ($va_info as $id => $data) {
+                if ($data['type'] == 'WMC') {
+                    $type_reps[] = $data['name'];
+                }
+            }
+            if (count($type_reps) == 1) {
+                print "<CONSTITUENCY_NAME>";
+                print $type_reps[0];
+                print "</CONSTITUENCY_NAME>\n";
+            }
+        } else {
+            print "<ERROR>";
+            print htmlspecialchars($va_info->text);
+            print "</ERROR>\n";
+        }
+    } else {
+            print "<ERROR>";
+            print htmlspecialchars($voting_areas->text);
+            print "</ERROR>\n";
+    }
+}
+
+?>
+</FAXYOURMP>
