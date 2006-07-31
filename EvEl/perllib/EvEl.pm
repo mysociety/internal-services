5ecea07c54573a0bd656360ef3fe25f8185728fb
chris <chris>
2006-07-31 09:59:58 +0000

    Use email functions from mySociety::Email (should make same change to FYR::Queue too).

--- EvEl/perllib/EvEl.pm
+++ EvEl/perllib/EvEl.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: EvEl.pm,v 1.42 2006-07-03 09:51:25 francis Exp $
+# $Id: EvEl.pm,v 1.43 2006-07-31 09:59:58 chris Exp $
 #
 
 package EvEl::Error;
@@ -39,6 +39,7 @@ use utf8;
 
 use mySociety::Config;
 use mySociety::DBHandle qw(dbh);
+use mySociety::Email;
 use mySociety::Util qw(random_bytes print_log is_valid_email);
 
 BEGIN {
@@ -285,83 +286,6 @@ sub recipient_id ($) {
     return $id;
 }
 
-
-# XXX next two functions copied from FYR::Queue...
-
-# format_mimewords STRING
-# Return STRING, formatted for inclusion in an email header.
-sub format_mimewords ($) {
-    my ($text) = @_;
-    # This is unpleasant. Whitespace which separates two encoded-words is not
-    # significant, so we need to fold it in to one of them. Rather than having
-    # some complicated state-machine driven by words, just encode the whole
-    # line if it contains any non-ASCII characters. However, this is going to
-    # suck whatever happens, because we can't include a blank in a
-    # quoted-printable MIME-word, so we have to encode it as =20 or whatever,
-    # so this is still going to be near-unreadable for users whose MUAs suck
-    # at MIME.
-    utf8::encode($text); # turn to string of bytes
-    if ($text =~ m#[\x00-\x1f\x80-\xff]#) {
-        $text =~ s#(\s|[\x00-\x1f\x80-\xff])#sprintf('=%02x', ord($1))#ge;
-        $text = "=?UTF-8?Q?$text?="
-    }
-    utf8::decode($text);
-    return $text;
-}
-
-# format_email_address NAME ADDRESS
-# Return a suitably MIME-encoded version of "NAME <ADDRESS>" suitable for use
-# in an email From:/To: header.
-sub format_email_address ($$) {
-    my ($name, $addr) = @_;
-    $name = format_mimewords($name);
-    $name =~ s/"/\\"/g;
-    $name =~ s/\\/\\\\/g;
-    $name = "\"$name\"";
-    return sprintf('%s <%s>', $name, $addr);
-}
-
-sub do_one_substitution ($$) {
-    my ($p, $n) = @_;
-    throw EvEl::Error("Substitution parameter '$n' is not present")
-        unless (exists($p->{$n}));
-    throw EvEl::Error("Substitution parameter '$n' is not defined")
-        unless (defined($p->{$n}));
-    return $p->{$n};
-}
-
-# do_template_substitution TEMPLATE PARAMETERS
-#
-sub do_template_substitution ($$) {
-    my ($body, $params) = @_;
-    $body =~ s#<\?=\$values\['([^']+)'\]\?>#do_one_substitution($params, $1)#ges;
-
-    my $subject;
-    if ($body =~ m#^Subject: ([^\n]*)\n\n#s) {
-        $subject = $1;
-        $body =~ s#^Subject: ([^\n]*)\n\n##s;
-    }
-
-    # Merge paragraphs into their own line.  Two blank lines separates a
-    # paragraph.
-    #$body =~ s#(?<!\n)\s*\n\s*(?!\n)# #gs;
-    #$body =~ s#\s*\n\s*(?!\n)# #gs;
-    #$body =~ s#(^|[^\n])\s*\n\s*($|[^\n])# #g;
-    #$body =~ s#\n\n#PARAGRAPHBREAK#g; $body =~ s#\n# #g; $body =~ s#PARAGRAPHBREAK#\n\n#g;
-    $body =~ s#(^|[^\n])[ \t]*\n[ \t]*($|[^\n])#$1 $2#g;
-
-    # Wrap text to 72-column lines.
-    local($Text::Wrap::columns = 69);
-    local($Text::Wrap::huge = 'overflow');
-    my $wrapped = Text::Wrap::wrap('     ', '     ', $body);
-    $wrapped =~ s/^\s+$//mg;
-
-#binmode(STDERR, ":utf8");
-#warn "Subject = $subject\n";
-
-    return ($subject, $wrapped);
-}
-
 #
 # Interface
 #
@@ -453,7 +377,7 @@ sub construct_email ($) {
         throw EvEl::Error("Template parameters '_parameters_' must be an associative array")
             if (ref($p->{_parameters_}) ne 'HASH');
         
-        (my $subject, $p->{_body_}) = do_template_substitution($p->{_template_}, $p->{_parameters_});
+        (my $subject, $p->{_body_}) = mySociety::Email::do_template_substitution($p->{_template_}, $p->{_parameters_});
         delete($p->{_template_});
         delete($p->{_parameters_});
 
@@ -463,7 +387,7 @@ sub construct_email ($) {
     throw EvEl::Error("missing field 'Subject' in MESSAGE") if (!exists($p->{Subject}));
 
     my %hdr;
-    $hdr{Subject} = format_mimewords($p->{Subject});
+    $hdr{Subject} = mySociety::Email::format_mimewords($p->{Subject});
 
     # To: and Cc: are address-lists.
     foreach (qw(To Cc)) {
@@ -472,7 +396,7 @@ sub construct_email ($) {
         if (ref($p->{$_}) eq '') {
             # Interpret as a literal string in UTF-8, so all we need to do is
             # escape it.
-            $hdr{$_} = format_mimewords($p->{$_});
+            $hdr{$_} = mySociety::Email::format_mimewords($p->{$_});
         } elsif (ref($p->{$_}) eq 'ARRAY') {
             # Array of addresses or [address, name] pairs.
             my @a = ( );
@@ -482,7 +406,7 @@ sub construct_email ($) {
                 } elsif (ref($_) ne 'ARRAY' || @$_ != 2) {
                     throw EvEl::Error("Element of '$_' field should be string or 2-element array");
                 } else {
-                    push(@a, format_email_address($_->[1], $_->[0]));
+                    push(@a, mySociety::Email::format_email_address($_->[1], $_->[0]));
                 }
             }
             $hdr{$_} = join(', ', @a);
@@ -497,7 +421,7 @@ sub construct_email ($) {
         } elsif (ref($p->{From}) ne 'ARRAY' || @{$p->{From}} != 2) {
             throw EvEl::Error("'From' field should be string or 2-element array");
         } else {
-            $hdr{From} = format_email_address($p->{From}->[1], $p->{From}->[0]);
+            $hdr{From} = mySociety::Email::format_email_address($p->{From}->[1], $p->{From}->[0]);
         }
     }
 
