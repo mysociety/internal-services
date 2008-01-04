7cacb74c87a8914a1bd44c5b360c502c9535bca2
etienne <etienne>
2008-01-04 00:30:30 +0000

    Minor bug fix re: months display

--- lib/BBCParl/Web.pm
+++ lib/BBCParl/Web.pm
@@ -495,15 +495,15 @@ sub get_programme_listing_html {
     my $time = '';
     my $ymd = '';
 
-    my %months = ('1' => 'January',
-		  '2' => 'February',
-		  '3' => 'March',
-		  '4' => 'April',
-		  '5' => 'May',
-		  '6' => 'June',
-		  '7' => 'July',
-		  '8' => 'August',
-		  '9' => 'September',
+    my %months = ('01' => 'January',
+		  '02' => 'February',
+		  '03' => 'March',
+		  '04' => 'April',
+		  '05' => 'May',
+		  '06' => 'June',
+		  '07' => 'July',
+		  '08' => 'August',
+		  '09' => 'September',
 		  '10' => 'October',
 		  '11' => 'November',
 		  '12' => 'December');
