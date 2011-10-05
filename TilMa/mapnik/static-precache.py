be37bcda67026d021ad4e797a3d013297b7cae4e
matthew <matthew>
2011-10-05 15:01:33 +0000

    Working script, all of UK 11 and 12.

--- TilMa/mapnik/static-precache.py
+++ TilMa/mapnik/static-precache.py
@@ -2,10 +2,14 @@
 #
 # This will precache some tiles that are used by the static maps.
 
+import sys
+
 MIN_ZOOM = 11
 MAX_ZOOM = 12
 XML_DIR = '/data/vhost/tilma.mysociety.org/osm/'
 CACHE_DIR = '/data/vhost/tilma.mysociety.org/tilecache/'
+
+sys.path.append('/data/vhost/tilma.mysociety.org/mapnik/')
 
 from generate_tiles import render_tiles
 
@@ -13,7 +17,7 @@ from generate_tiles import render_tiles
 #bbox = (-180.0,-90.0, 180.0,90.0)
 #render_tiles(bbox, mapfile, tile_dir, 0, 5, "World")
 
-bbox = (-0.5,51.25, 0.5,51.75)
-render_tiles(bbox, XML_DIR + 'mapumental-map.xml', CACHE_DIR + 'mapumental-map/', MIN_ZOOM, MAX_ZOOM)
-render_tiles(bbox, XML_DIR + 'mapumental-names.xml', CACHE_DIR + 'mapumental-names/', MIN_ZOOM, MAX_ZOOM)
+bbox = (-6,49.95, 1.75,60)
+for layer in ('mapumental-map', 'mapumental-names', 'osm'):
+    render_tiles(bbox, XML_DIR + layer + '.xml', CACHE_DIR + layer + '/', MIN_ZOOM, MAX_ZOOM)
 
