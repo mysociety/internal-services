2c36c7b8c4a47abbd71c521f0cc297099995cd8b
dademcron <dademcron>
2012-10-03 13:15:59 +0000

    Try and update queue when contact detail edited.

--- mapit-dadem-loading/CouncilMatch.pm
+++ mapit-dadem-loading/CouncilMatch.pm
@@ -7,7 +7,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: CouncilMatch.pm,v 1.22 2010-10-05 15:55:17 dademcron Exp $
+# $Id: CouncilMatch.pm,v 1.23 2012-10-03 13:15:59 dademcron Exp $
 #
 
 package CouncilMatch;
@@ -18,6 +18,8 @@ use HTML::TokeParser;
 use Text::CSV;
 use URI;
 use File::Slurp;
+
+use FYRQueue;
 
 use mySociety::MaPit;
 use mySociety::Parties;
@@ -126,6 +128,7 @@ sub refresh_live_data($$) {
     # Go through all updated data, and either insert or replace
     foreach $update_key (keys %$update_keys) {
         my $row = $update_keys->{$update_key};
+        my $rep_id;
         if (exists($current_keys->{$update_key})) {
             # update
             my $rows_affected = $d_dbh->do(q#update representative set
@@ -144,6 +147,7 @@ sub refresh_live_data($$) {
                 . " method: " . $row->{method}
                 . " fax: " . $row->{rep_fax} . " email: " . $row->{rep_email} 
                 . "\n";
+            $rep_id = $current_keys->{$update_key}->{id};
         } else {
             # insert into
             $d_dbh->do(q#insert into representative 
@@ -159,7 +163,15 @@ sub refresh_live_data($$) {
                 . " method: " . $row->{method}
                 . " fax: " . $row->{rep_fax} . " email: " . $row->{rep_email} 
                 . "\n";
-
+            ($rep_id) = $d_dbh->selectrow_array('select id from representative where import_key=?', {}, $update_key);
+        }
+        if ($row->{method} eq 'either' || $row->{method} eq 'email') {
+            FYRQueue::admin_update_recipient($rep_id, $row->{rep_email}, 0);
+        } elsif ($row->{method} eq 'fax')
+            FYRQueue::admin_update_recipient($rep_id, $row->{rep_fax}, 0);
+        } elsif ($row->{method} eq 'via')
+            # Need to look up via contact here TODO
+            # FYRQueue::admin_update_recipient($rep_id, $row, 1);
         }
     }
 
