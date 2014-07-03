70979b0f41f86ad7f386e977133764749f1eba81
Mark Longair <mhl@pobox.com>
2014-07-03 19:28:58 +0100

    Update the script to make a CSV file of current MEPs
    The old script relied on a large regular expression to pick out data
    from the region page, which made it difficult to fix errors when the
    page structure changed. This commit introduces a version rewritten in
    Python which uses BeautifulSoup to extract data from pages - hopefully
    this will be easier to deal with in future.

---
+++ services/mapit-dadem-loading/scrapers/europarlUK.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# This script outputs a CSV file of current UK MEPs scraped from
+# http://www.europarl.org.uk/en/your_meps.html to standard output.
+
+from csv import DictWriter
+import hashlib
+import json
+import os
+import re
+import sys
+import urllib2
+import urlparse
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+cache_directory = 'cache'
+
+host = 'www.europarl.org.uk'
+regions_path = '/en/your_meps.html'
+
+expected_region_counts = {
+    'East Midlands': 5,
+    'Eastern': 7,
+    'London': 8,
+    'North East': 3,
+    'North West': 8,
+    'Northern Ireland': 3,
+    'Scotland': 6,
+    'South East': 10,
+    'South West': 6,
+    'Wales': 4,
+    'West Midlands': 7,
+    'Yorkshire and the Humber': 6
+}
+
+expected_regions = expected_region_counts.keys()
+
+def get_url_cached(url):
+    """GET a URL, caching the result for future runs of the script"""
+    cached_filename = os.path.join(
+        cache_directory,
+        hashlib.md5(url).hexdigest()
+    )
+    if os.path.exists(cached_filename):
+        with open(cached_filename) as f:
+            return f.read()
+    else:
+        response = urllib2.urlopen(url)
+        data = response.read()
+        with open(cached_filename, 'w') as f:
+            f.write(data)
+        return data
+
+def make_url(path, params=''):
+    return urlparse.urlunsplit(
+    ('http', host, path, params, '')
+)
+
+all_regions_url = make_url(regions_path)
+all_region_soup = BeautifulSoup(get_url_cached(all_regions_url))
+content_div = all_region_soup.find(id='content')
+
+def tidy_region_name(region_name):
+    result = region_name.replace(' Region', '')
+    return result.replace('Noth', 'North')
+
+csv_headers = [
+    'First', 'Last', 'Constituency', 'Party', 'Email', 'Fax', 'Image'
+]
+
+csv_writer = DictWriter(sys.stdout, fieldnames=csv_headers)
+csv_writer.writeheader()
+
+for region_link in content_div.find_all('a', {'class': 'simple'}):
+    region_name = tidy_region_name(region_link.text).strip()
+    region_url = make_url(region_link['href'])
+    region_soup = BeautifulSoup(get_url_cached(region_url))
+    meps_found = 0
+    for mep_soup in region_soup('div', {'class': re.compile('standard')}):
+        image_element = mep_soup.find('img')
+        # The name is in the span immediately after the image:
+        sibling_tags = [
+            i for i in image_element.next_siblings
+            if isinstance(i, Tag)
+        ]
+        name_match = re.search(r'^\s*(\S+)\s+(.*)', sibling_tags[0].text)
+        # Assume there's only one first name and possibly multiple
+        # last names, as the previous script did:
+        first_name, last_names = name_match.groups()
+        # Look for the party affiliation:
+        party_match = re.search(
+            r'(?ims)National\s+party\s*:\s*(.*?)\n',
+            mep_soup.text
+        )
+        if party_match:
+            party = party_match.group(1).strip()
+        else:
+            message = "Warning: couldn't find the party for {0} {1}"
+            print >> sys.stderr, message.format(first_name, last_names)
+            party = ''
+        # Find any email addreses - in fact, we only use the first
+        # one.
+        mailto_links = mep_soup.find_all(
+            'a',
+            {'href': re.compile(r'^mailto:')}
+        )
+        email_addresses = [
+            re.sub(r'^mailto:', '', a['href']).strip()
+            for a in mailto_links
+        ]
+        row = {
+            'Image': make_url(image_element['src']),
+            'First': first_name.strip().encode('utf-8'),
+            'Last': last_names.strip().encode('utf-8'),
+            'Constituency': region_name,
+            'Party': party.encode('utf-8'),
+            'Email': email_addresses[0],
+            'Fax': ''
+        }
+        csv_writer.writerow(row)
+        meps_found += 1
+    if meps_found != expected_region_counts[region_name]:
+        message = "Unexpected number of MEPs found for {0}: expected {1}, but found {2}"
+        print >> sys.stderr, message.format(
+            region_name, expected_region_counts[region_name], meps_found
+        )
