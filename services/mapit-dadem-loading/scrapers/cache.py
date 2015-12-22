f275adef361186b8b54ca58952bcd49b61e6bc56
DaDem cron jobs <dademcron@leopard.ukcod.org.uk>
2015-12-22 14:03:33 +0000

    Fix London scraper.

--- services/mapit-dadem-loading/scrapers/cache.py
+++ services/mapit-dadem-loading/scrapers/cache.py
@@ -10,8 +10,8 @@ class DiskCacheFetcher:
         filename = hashlib.md5(url).hexdigest()
         filepath = os.path.join(self.cache_dir, filename)
         if os.path.exists(filepath) and time.time() - os.path.getmtime(filepath) < 3600:
-            return open(filepath).read()
+            return open(filepath).read().decode('utf-8')
         data = urlopen(url).read()
         with open(filepath, 'w') as fp:
             fp.write(data)
-        return data
+        return data.decode('utf-8')
